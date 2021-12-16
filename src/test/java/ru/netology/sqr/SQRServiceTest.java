6a03559d8eff2b426a8b1bd99006ae765990ef52
Dudkina Olga <dudkinaolia@yandex.ru>
2021-12-17 00:08:10 +0300

    adding tasks

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -16,6 +16,42 @@ class SQRServiceTest {
         
         assertEquals( expected, actual);
     }
+    @org.junit.jupiter.api.Test
+    void shouldSqrtCalculate2 () {
+        SQRService service = new SQRService();
+
+        int initialBoundary = 1;
+        int endBoundary = 100;
+        int expected = 1;
+        int actual = service.sqrtCalculate(initialBoundary,endBoundary);
+
+        assertEquals( expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldSqrtCalculate3 () {
+        SQRService service = new SQRService();
+
+        int initialBoundary = 1;
+        int endBoundary = 99;
+        int expected = 0;
+        int actual = service.sqrtCalculate(initialBoundary, endBoundary);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldSqrtCalculate4 () {
+        SQRService service = new SQRService();
+
+        int initialBoundary = 100;
+        int endBoundary = 10000;
+        int expected = 90;
+        int actual = service.sqrtCalculate(initialBoundary, endBoundary);
+
+        assertEquals(expected, actual);
+    }
+
 
 
     }
